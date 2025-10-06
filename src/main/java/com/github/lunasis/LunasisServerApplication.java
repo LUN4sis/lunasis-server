7b3f1a5c66defd66ad3c9d29bd4fa95fb1dec9ef
kimmoongi <moongi0320@naver.com>
2025-10-06 22:25:04 +0900

    Initial commit

---
+++ src/main/java/com/github/lunasis/LunasisServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.lunasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunasisServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunasisServerApplication.class, args);
+    }
+
+}
