f792e98e3564e8b5501606aac0cdf974a614ee97
kimmoongi <moongi0320@naver.com>
2025-10-06 23:14:21 +0900

    feat(user): 기본 user 엔티티 생성

---
+++ src/main/java/com/github/lunasis/domain/user/entity/User.java
@@ -0,0 +1,34 @@
+package com.github.lunasis.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Builder
+@Getter
+@Table(name = "User")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(name = "google_id", nullable = false, unique = true)
+    private String googleId;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "profile")
+    private String profile;
+
+
+}
