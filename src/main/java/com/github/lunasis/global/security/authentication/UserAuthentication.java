064216b8932979595ff637e1eacdcfc1f17470c0
kimmoongi <moongi0320@naver.com>
2025-10-07 00:02:59 +0900

    feat(jwt): 커스텀 authentication 객체 생성

---
+++ src/main/java/com/github/lunasis/global/security/authentication/UserAuthentication.java
@@ -0,0 +1,46 @@
+package com.github.lunasis.global.security.authentication;
+
+import com.github.lunasis.domain.user.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserAuthentication implements Authentication {
+
+    private final User user;
+
+    @Getter
+    @Setter
+    boolean authenticated;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return user;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return user.getNickname();
+    }
+}
