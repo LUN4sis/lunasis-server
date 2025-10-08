bebde30163ad9a29dc38f7e7621b30bd097a4918
kimmoongi <moongi0320@naver.com>
2025-10-08 21:38:26 +0900

    feat(jwt): util 클래스에 토큰 생성 로직 추가

--- src/main/java/com/github/lunasis/global/security/jwt/JwtUtil.java
+++ src/main/java/com/github/lunasis/global/security/jwt/JwtUtil.java
@@ -4,7 +4,11 @@ package com.github.lunasis.global.security.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.github.lunasis.domain.auth.entity.ExchangeToken;
+import com.github.lunasis.domain.auth.entity.RefreshToken;
 import com.github.lunasis.domain.auth.exceptions.AuthExceptions;
+import com.github.lunasis.domain.auth.repository.ExchangeTokenRepository;
+import com.github.lunasis.domain.auth.repository.RefreshTokenRepository;
 import com.github.lunasis.domain.user.entity.User;
 import com.github.lunasis.global.property.JwtProperty;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,6 +26,9 @@ import java.util.UUID;
 public class JwtUtil {
 
     private final JwtProperty jwtProperty;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final ExchangeTokenRepository exchangeTokenRepository;
+
 
     @Bean
     public Algorithm algorithm() {
@@ -71,13 +78,45 @@ public class JwtUtil {
         );
     }
 
-    public String generateToken(User user) {
+    public String generateAccessToken(User user) {
         return JWT.create()
                 .withIssuedAt(Instant.now())
                 .withExpiresAt(Instant.now().plus(jwtProperty.getTokenExpiration(), ChronoUnit.HOURS))
                 .withClaim("id", user.getId().toString())
                 .withClaim("type", TokenType.ACCESS.name())
                 .sign(algorithm());
+    }
+
+    public String generateRefreshToken(User user) {
+
+        String token = JWT.create()
+                .withIssuedAt(Instant.now())
+                .withExpiresAt(Instant.now().plus(jwtProperty.getRefreshExpiration(), ChronoUnit.HOURS))
+                .withClaim("id", user.getId().toString())
+                .withClaim("type", TokenType.REFRESH.name())
+                .sign(algorithm());
+
+        RefreshToken refreshToken = RefreshToken.builder()
+                .userId(user.getId())
+                .refreshToken(token)
+                .ttl(jwtProperty.getRefreshExpiration())
+                .build();
+
+        refreshTokenRepository.save(refreshToken);
+
+        return token;
+    }
+
+    public String generateExchangeToken(String accessToken, String refreshToken, boolean firstLogin) {
+
+        ExchangeToken exchangeToken = exchangeTokenRepository.save(ExchangeToken.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .firstLogin(firstLogin)
+                .ttl(5)
+                .build());
+
+        return exchangeToken.getId();
     }
 
 
