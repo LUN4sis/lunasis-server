b09f3b4212e572747d17563a4924ae19547ed8e3
kimmoongi <moongi0320@naver.com>
2025-10-09 17:59:56 +0900

    feat(exception): 예외 처리 handler 추가

---
+++ src/main/java/com/github/lunasis/global/exception/ApiExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.github.lunasis.global.exception;
+
+import com.github.lunasis.global.dto.ApiResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse<?> handleValidation(MethodArgumentNotValidException e) {
+        FieldError error = e.getBindingResult().getFieldError();
+        String message = (error != null && error.getDefaultMessage() != null)
+                ? error.getDefaultMessage() : "잘못된 요청입니다.";
+        return ApiResponse.error(message, 404);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ApiResponse<Void> AccessDeniedException(AccessDeniedException e) {
+        return ApiResponse.error("권한이 없습니다.", 403);
+    }
+
+    @ExceptionHandler(ApiException.class)
+    public ApiResponse<?> handleApiException(ApiException e) {
+        return ApiResponse.error(e.getMessage(), e.getCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<?> handleException(Exception e) {
+        return ApiResponse.error("알 수 없는 오류가 발생했습니다.", 500);
+    }
+
+}
