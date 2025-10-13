95d34c3eee3b84f06309e6302b5e7516e9ddf934
kimmoongi <moongi0320@naver.com>
2025-10-13 19:49:48 +0900

    chore(user): user 엔티티에 chatRoom 연관관계 설정

--- src/main/java/com/github/lunasis/domain/user/entity/User.java
+++ src/main/java/com/github/lunasis/domain/user/entity/User.java
@@ -1,13 +1,11 @@
 package com.github.lunasis.domain.user.entity;
 
+import com.github.lunasis.domain.chat.entity.ChatRoom;
 import com.github.lunasis.domain.user.dto.request.UpdateUserInfo;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Entity
 @Builder
@@ -17,39 +15,32 @@ import java.util.UUID;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    List<ChatRoom> chatRooms;
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(nullable = false, updatable = false)
     private UUID id;
-
     @Column(unique = true)
     private String nickname;
-
     @Column(name = "google_id", nullable = false, unique = true)
     private String googleId;
-
     @Column(name = "age")
     private Integer age;
-
     @Column(name = "profile")
     private String profile;
-
     @Column(name = "private_chat")
     @Builder.Default
     private Boolean privateChat = false;
-
     @Enumerated(EnumType.STRING)
     @Column(name = "insurance")
     private Insurance insurance;
-
     @Column(name = "zip_code")
     private String zipCode;
-
     // 선호조사 용
     @Enumerated(EnumType.STRING)
     @Column(name = "community")
     private Community community;
-
     @ElementCollection(targetClass = ProductCategory.class)
     @CollectionTable(
             name = "user_product_categories",
@@ -59,7 +50,6 @@ public class User {
     @Column(name = "product_category")
     @Builder.Default
     private Set<ProductCategory> productCategory = new HashSet<>();
-
     //선호조사 용
     @Column(name = "price_comparison")
     @Builder.Default
