a303a440f792d49d612acfca60b97f4e392b9c25
Kimmoongi0320 <moongi0320@naver.com>
2025-11-18 17:06:46 +0900

    chore(post): swagger 적용

--- src/main/java/com/github/lunasis/domain/post/controller/PostController.java
+++ src/main/java/com/github/lunasis/domain/post/controller/PostController.java
@@ -5,10 +5,12 @@ import com.github.lunasis.domain.post.dto.response.PostInfoResponse;
 import com.github.lunasis.domain.post.service.PostService;
 import com.github.lunasis.domain.user.entity.User;
 import com.github.lunasis.global.dto.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,13 +26,15 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
+    @Operation(description = " 새 게시물 생성 ")
     public ApiResponse<PostInfoResponse> createPost(@AuthenticationPrincipal User user,
                                                     @RequestBody CreatePostRequest request) {
 
         return ApiResponse.ok(postService.createPost(user.getId(), request));
     }
 
-    @PostMapping("/{postId}")
+    @GetMapping("/{postId}")
+    @Operation(description = " 게시물 상세 조회 ")
     public ApiResponse<PostInfoResponse> getPost(@AuthenticationPrincipal User user, @PathVariable UUID postId) {
 
         return ApiResponse.ok(postService.getPost(user, postId));
