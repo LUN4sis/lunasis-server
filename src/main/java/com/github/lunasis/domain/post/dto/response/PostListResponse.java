a92e4fcfd0fe7f3100b5d65c831689b4a718823d
kimmoongi <moongi0320@naver.com>
2025-11-23 17:24:12 +0900

    feat(post): 게시글 목록 api 요청 구현

---
+++ src/main/java/com/github/lunasis/domain/post/dto/response/PostListResponse.java
@@ -0,0 +1,29 @@
+package com.github.lunasis.domain.post.dto.response;
+
+import com.github.lunasis.domain.post.entity.Post;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Builder;
+
+@Builder
+public record PostListResponse(
+        UUID postId,
+        SimpleAuthorResponse author,
+        String title,
+        String content,
+        int commentCount,
+        boolean isBookmarked,
+        LocalDateTime createdAt
+) {
+    public static PostListResponse from(Post post, boolean isBookmarked) {
+        return PostListResponse.builder()
+                .postId(post.getId())
+                .author(SimpleAuthorResponse.from(post.getUser()))
+                .title(post.getTitle())
+                .content(post.getContent())
+                .commentCount(post.getComments().size())
+                .isBookmarked(isBookmarked)
+                .createdAt(post.getCreatedAt())
+                .build();
+    }
+}
