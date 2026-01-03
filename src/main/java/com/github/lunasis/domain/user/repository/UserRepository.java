db380a5988a73eb67a2f9ac08150c68d358efee6
kimmoongi <moongi0320@naver.com>
2026-01-04 00:12:23 +0900

    chore(user): 여러개의 OAuth로그인 구현을 위한 필드값 수정

--- src/main/java/com/github/lunasis/domain/user/repository/UserRepository.java
+++ src/main/java/com/github/lunasis/domain/user/repository/UserRepository.java
@@ -1,13 +1,12 @@
 package com.github.lunasis.domain.user.repository;
 
 import com.github.lunasis.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByGoogleId(String googleId);
+    Optional<User> findByOauthId(String oauthId);
 
     Optional<User> findByNickname(String nickname);
 }
