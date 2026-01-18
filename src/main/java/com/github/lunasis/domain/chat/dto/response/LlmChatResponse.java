12580c323e522ce8b013d1a3ba4ba6bf644ff877
kimmoongi <moongi0320@naver.com>
2026-01-18 16:46:12 +0900

    feat(chat): LLM 응답 response 생성

---
+++ src/main/java/com/github/lunasis/domain/chat/dto/response/LlmChatResponse.java
@@ -0,0 +1,10 @@
+package com.github.lunasis.domain.chat.dto.response;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record LlmChatResponse(
+        @NotNull String answer,
+        @NotNull @Size(min = 1) float[] embedding
+) {
+}
