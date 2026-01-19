3b9366e54e6db0d786d3f403cf8094460cf7f800
kimmoongi <moongi0320@naver.com>
2026-01-19 22:41:09 +0900

    refactor(chat): 필드 유형 변경

--- src/main/java/com/github/lunasis/domain/user/entity/ChatSetting.java
+++ src/main/java/com/github/lunasis/domain/user/entity/ChatSetting.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,7 +25,7 @@ public class ChatSetting {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    private UUID id;
 
     @Column(name = "warmth")
     @Enumerated(EnumType.STRING)
