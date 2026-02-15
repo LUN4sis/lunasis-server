66fb630652303165300f9d67f284366abb4c0b5c
Kimmoongi0320 <moongi0320@naver.com>
2026-02-15 15:39:44 +0900

    refactor(chat): 불필요한 PreAuthorize 어노테이션 제거

--- src/main/java/com/github/lunasis/domain/chat/service/ChatService.java
+++ src/main/java/com/github/lunasis/domain/chat/service/ChatService.java
@@ -27,7 +27,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -35,7 +34,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-@PreAuthorize("isAuthenticated()")
 public class ChatService {
 
     @Value("${fastapi.url}")
